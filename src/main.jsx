05b449d0bc4a91c17dc5719cbbdfa4df8888952d
javieryami <javieryami1@outlook.com>
2025-05-04 20:47:50 -0600

    tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
-import App from './App.jsx'
+import Landing from './views/landing/landing.jsx';
+import Login from './views/login/login.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Landing/>}/>
+      <Route path='/login' element={<Login></Login>} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/views/login/login.jsx
@@ -0,0 +1,12 @@
+const Login = () => {
+
+    return (
+        <>
+        <div>
+            <h1>Hola soy el login</h1>
+        </div>
+        </>
+    )
+};
+
+export default Login; 
